b4fef727b5080d9ee5e6da51a607d2aee7cf7d9c
Vasiliy Ahmedzhanov <ahmedzhanov.v2@wb.ru>
2023-02-02 16:36:01 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// ЗАДАНИЕ:
+// * сделать из плохого кода хороший;
+// * важно сохранить логику появления ошибочных тасков;
+// * сделать правильную мультипоточность обработки заданий.
+// Обновленный код отправить через merge-request.
+
+// приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
+// В конце должно выводить успешные таски и ошибки выполнены остальных тасков
+
+// A Ttype represents a meaninglessness of our life
+type Ttype struct {
+	id         int
+	cT         string // время создания
+	fT         string // время выполнения
+	taskRESULT []byte
+	result     bool
+}
+
+func main() {
+	superChan := make(chan Ttype, 10)
+	defer close(superChan)
+
+	go taskCreator(superChan)
+
+	doneTasks := make(chan Ttype)
+	defer close(doneTasks)
+	undoneTasks := make(chan error)
+	defer close(undoneTasks)
+
+	go func() {
+		// получение тасков
+		for t := range superChan {
+			t = taskWorker(t)
+			go taskSorter(t, doneTasks, undoneTasks)
+		}
+	}()
+
+	for {
+		select {
+		case r := <-doneTasks:
+			fmt.Printf("Done: %d \n", r.id)
+		case r := <-undoneTasks:
+			fmt.Printf("Undone: %s \n", r)
+		}
+	}
+}
+
+func taskCreator(a chan Ttype) {
+	go func() {
+		for {
+			cT := time.Now().Format(time.RFC3339)
+			if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+				cT = "Some error occured"
+			}
+			a <- Ttype{cT: cT, id: int(time.Now().Unix())} // передаем таск на выполнение
+		}
+	}()
+}
+
+func taskWorker(a Ttype) Ttype {
+	tt, _ := time.Parse(time.RFC3339, a.cT)
+	if tt.After(time.Now().Add(-20 * time.Second)) {
+		a.taskRESULT = []byte("task has been successed")
+		a.result = true
+	} else {
+		a.taskRESULT = []byte("something went wrong")
+		a.result = false
+	}
+	a.fT = time.Now().Format(time.RFC3339Nano)
+
+	time.Sleep(time.Millisecond * 150)
+
+	return a
+}
+
+func taskSorter(t Ttype, doneTasks chan Ttype, undoneTasks chan error) {
+	if t.result {
+		doneTasks <- t
+	} else {
+		undoneTasks <- fmt.Errorf("task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+	}
+}
